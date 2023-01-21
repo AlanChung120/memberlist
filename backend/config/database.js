b7c52aaab84d5329367d6e3018d66c14c27ad559
Alan Chung <58128141+AlanChung120@users.noreply.github.com>
2023-01-21 11:21:30 -0500

    Create database.js

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+import { Sequelize } from "sequelize";
+
+// get data
+const datab = '' // enter the name of the database
+const password = '' // enter your own password for MySQL
+const db = new Sequelize(datab, 'root', password, {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+export default db;
