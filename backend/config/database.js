cb6f38e99c09c8ec03a8b074d18bf11527c76d53
Alan Chung <58128141+AlanChung120@users.noreply.github.com>
2023-01-31 09:56:34 -0500

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // get data
-const datab = '' // enter the name of the database
-const password = '' // enter your own password for MySQL
+const datab = ''; // enter the name of the database
+const password = ''; // enter your own password for MySQL
 const db = new Sequelize(datab, 'root', password, {
     host: "localhost",
     dialect: "mysql"
